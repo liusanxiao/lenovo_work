9aa374543ea668c5fd6aa4485d74ac0e9f04a791
liusx <liusx5092@163.com>
2018-01-19 11:23:29 +0800

    第一次提交工程

---
+++ test/src/com/test/Test01.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Test01 {
+	public static void main(String[] args) {
+		System.out.println("asgdadfafd");
+	}
+
+	public void test() {
+		String string = new String();
+		string = "abcdefghijkl";
+	}
+}
